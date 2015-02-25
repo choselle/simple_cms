4696ae377375b7e302077160ce034fe149159861
Chase Hoselle <choselle@yahoo.com>
2015-02-25 15:48:54 -0600

    Update

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,16 @@ class DemoController < ApplicationController
   layout false
 
   def index
+  	render('hello')
   end
+
+  def hello
+  	# render('index')
+  	@array = [1, 2, 3, 4, 5]
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
 end
